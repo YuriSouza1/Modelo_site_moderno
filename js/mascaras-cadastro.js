a917c85b32119d7b862b572c48b57d4c6c5325a1
YuriSouza1 <106026914+YuriSouza1@users.noreply.github.com>
2023-11-16 21:09:36 -0300

    Add files via upload

---
+++ js/mascaras-cadastro.js
@@ -0,0 +1,11 @@
+function emailMask(email) {
+	var maskedEmail = email.replace(/([^@\.])/g, "*").split('');
+	var previous	= "";
+	for(i=0;i<maskedEmail.length;i++){
+		if (i<=1 || previous == "." || previous == "@"){
+			maskedEmail[i] = email[i];
+		}
+		previous = email[i];
+	}
+	return maskedEmail.join('');
+}
